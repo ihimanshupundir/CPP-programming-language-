fc3aed119c5aaa3c6a274eb1aa251c258e8c9f91
Himanshu Pundir <81055754+ihimanshupundir@users.noreply.github.com>
2023-04-22 01:45:47 +0530

    Printing 1st message

--- bacisofcpp.cpp
+++ bacisofcpp.cpp
@@ -12,20 +12,6 @@ But Also: We use return 0 intentionally in between a program if we want to skip 
 
 
 
-
-/*
-#include <iostream>
-
-int main() // void main() is not allowed in cpp 
-// here use   using namespace std;  alternate of   std::
-{
-	std:: cout<<"Hii hp";
-	// std::   is better than  using namespace std;
-	
-	return 0; // because we specified what kind of value this fun will return   ie  int main()
-} 
-*/
-
 /*
 #include <iostream>
 int main()
